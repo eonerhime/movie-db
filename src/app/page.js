97684299a6a40d436cdfcb3b8a9dae159506704f
Emo Onerhime <emo.onerhime@gmail.com>
2025-03-20 22:58:45 +0100

    Fetch and display movies and add Results and Card components

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,19 @@
-function Page() {
-  return <div className="uppercase">Movie DB</div>;
-}
+import Results from "@/components/Results";
 
-export default Page;
+const API_KEY = process.env.API_KEY;
+
+export default async function Home() {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const results = data.results;
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
+}
--- src/app/top/[genre]/page.js
+++ src/app/top/[genre]/page.js
@@ -1,5 +1,23 @@
-function page() {
-  return <div>Top Pages</div>;
-}
+import Results from "@/components/Results";
 
-export default page;
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ params }) {
+  const { genre } = await params;
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === "rated" ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const results = data.results;
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
+}
