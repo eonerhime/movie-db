48a46b7321e8ee53d967cb6ae4a5f45b5c1f1f51
Emo Onerhime <emo.onerhime@gmail.com>
2025-03-21 14:03:54 +0100

    Fixed  error handling component name from error to Error

--- src/app/error.js
+++ src/app/error.js
@@ -1,25 +1,18 @@
 "use client";
 
-import Link from "next/link";
 import { useEffect } from "react";
 
-export default function error({ error, reset }) {
-  // const navigate = use();
-
+export default function Error({ error, reset }) {
   useEffect(() => {
     console.log(error);
   }, [error]);
 
   return (
-    <div className="flex flex-col gap-6 justify-center items-center mx-auto max-h-full h-96">
+    <div className="text-center mt-10">
       <h1>Something went wrong. Please try again later.</h1>
-      <Link
-        href="/"
-        className="border-2 border-primary-500 p-4 px-8 rounded-lg hover:text-amber-600 cursor-pointer"
-        onClick={() => reset()}
-      >
+      <button className="hover:text-amber-600" onClick={() => reset()}>
         Try Again
-      </Link>
+      </button>
     </div>
   );
 }
