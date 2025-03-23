16ccda4b97b3fb27fe2dd70b008f622b70a264c1
Emo Onerhime <emo.onerhime@gmail.com>
2025-03-23 10:21:46 +0100

    Update environment variables and dependencies for Clerk and MongoDB

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,17 @@ import Results from "@/components/Results";
 
 const API_KEY = process.env.API_KEY;
 
-export default async function Home() {
+export default async function Page() {
   const res = await fetch(
     `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US&page=1`
   );
   const data = await res.json();
+  console.log(
+    await fetch(
+      `https://api.themoviedb.org/3/trending/all/week?api_key=5773c15b25863252d5fdf3b37994bd8a&language=en-US&page=1)`
+    )
+  );
+
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
