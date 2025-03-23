9185b52ef119581874aee6e6a8fa565d393187e1
Emo Onerhime <emo.onerhime@gmail.com>
2025-03-23 14:19:33 +0100

    Refactor error handling in Error component and update Page component to display home page content

--- src/app/error.js
+++ src/app/error.js
@@ -1,58 +1,21 @@
-import Results from "@/components/Results";
+"use client";
 
-const API_KEY = process.env.API_KEY;
+import { useEffect } from "react";
 
-export default async function Page() {
-  const res = await fetch(
-    `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US&page=1`
-  );
-  const data = await res.json();
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  const results = data.results;
-  let homePageContent = null;
-  try {
-    const results = await fetch(process.env.URL + "/api/homepagecontent/get", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      cache: "no-store",
-    });
-
-    if (!results.ok) {
-      throw new Error("Failed to fetch data");
-    }
-
-    const text = await results.text();
-    if (text) {
-      const data = JSON.parse(text);
-      homePageContent = data[0] || null;
-    } else {
-      console.log("Empty response");
-    }
-  } catch (error) {
-    console.log("Error getting the home page content", error);
-  }
+export default function Error({ error, reset }) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
 
   return (
-    <div>
-      {homePageContent && (
-        <div className="text-center mb-10 max-w-6xl mx-auto py-10">
-          <h1 className="text-4xl font-extrabold mb-4 bg-gradient-to-r from-purple-600 via-pink-500 to-red-500 bg-clip-text text-transparent">
-            {homePageContent.title}
-          </h1>
-          <div
-            className="sm:text-lg p-4"
-            dangerouslySetInnerHTML={{ __html: homePageContent.description }}
-          ></div>
-        </div>
-      )}
-      <div>
-        <Results results={results} />
-      </div>
+    <div className="text-center mt-10">
+      <h1>Something went wrong. Please try again later.</h1>
+      <button
+        className="mt-4 border-2 border-grey-50 p-4 rounded-lg hover:text-amber-600"
+        onClick={() => reset()}
+      >
+        Try Again
+      </button>
     </div>
   );
 }
